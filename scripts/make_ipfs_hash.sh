61790df8257f85812910e9e04a60992cad204a7a
JAremko <w3techplayground@gmail.com>
2019-02-20 19:17:57 +0200

    Auto release with TravisCI

---
+++ scripts/make_ipfs_hash.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+ipfs daemon --init --offline & > /tmp/ipfs_daemon.log
+
+while [ ! -e ${HOME}/.ipfs/api ]
+do
+    echo "Waiting for IPFS daemon to start";
+    sleep 1;
+done
+
+echo "IPFS daemon started."
+
+ipfs add --recursive --quiet --only-hash ./packages \
+    | tail -1 \
+    | tr -d '\n' \
+    > ipfs_root
